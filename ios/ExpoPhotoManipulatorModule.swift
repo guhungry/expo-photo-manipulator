b485e28796f1a7c737400d57c9a351ab4fc918b2
Woraphot Chokratanasombat <guhungry@gmail.com>
2024-08-09 00:39:02 +0700

    feature(ios): add WCPhotoManipulator in podspec

--- ios/ExpoPhotoManipulatorModule.swift
+++ ios/ExpoPhotoManipulatorModule.swift
@@ -18,11 +18,6 @@ public class ExpoPhotoManipulatorModule: Module {
     // Defines event names that the module can send to JavaScript.
     Events("onChange")
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
-    }
-
     // Defines a JavaScript function that always returns a Promise and whose native code
     // is by default dispatched on the different thread than the JavaScript runtime runs on.
     AsyncFunction("setValueAsync") { (value: String) in
@@ -30,15 +25,6 @@ public class ExpoPhotoManipulatorModule: Module {
       self.sendEvent("onChange", [
         "value": value
       ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ExpoPhotoManipulatorView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: ExpoPhotoManipulatorView, prop: String) in
-        print(prop)
-      }
     }
   }
 }
